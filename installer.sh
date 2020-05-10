508d8a6e5276a788a45a438fee485344d5c19fba
tkdrob <tkdrob4390@yahoo.com>
2020-05-09 20:19:30 -0400

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -14,11 +14,6 @@ warn "via a Docker container."
 warn ""
 echo 'Please typ "not supported" to continue this installation'
 read x
-if [ "$x" != "not supported" ]
-then
-  echo "OK, bye!"
-  exit 1
-fi
 
 ARCH=$(uname -m)
 DOCKER_BINARY=/usr/bin/docker
